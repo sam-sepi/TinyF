0915a6b494b68b987bb4637b019970af65e9cee9
sam-sepi <sam-sepi.84@gmail.com>
2020-01-27 16:03:03 +0100

    Update PdoWrapper.php

--- database/PdoWrapper.php
+++ database/PdoWrapper.php
@@ -56,5 +56,100 @@ class PdoWrapper
             }
         }
     }
+    
+    /**
+     * @fn find 
+     * Fetch single row
+     * Ex.: $conn->find("SELECT title FROM qutes WHERE id = ?", [43]);
+     *
+     * @param  string $sql
+     * @param  array|null  $where
+     * 
+     * An array of values with as many elements as there are bound 
+     * parameters in the SQL statement being executed. 
+     * All values are treated as PDO::PARAM_STR.
+     * 
+     * @return The return value of this function on success depends on the fetch type.
+     */
+    public function find(string $sql, array $where = null)
+    {
+        if($this->pdo instanceof \PDO)
+        {
+            try {
+                
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute($where);
+                
+                return $stmt->fetch();
+            }
+            catch (\PDOException $e)
+            {
+                //Logger in dev or prod. mode
+                if($this->dev)
+                {
+                    print($e->getMessage());
+                }
+                else
+                {
+                    $f = fopen('db_logger.txt', "a", dirname(__FILE__));
+                    fwrite($f, $e->getMessage() . "\r\n");
+                    fclose($f);
+                }
+            }        
+        }    
+    }
+
+    /**
+     * @fn run 
+     * Run Query
+     * 
+     * Ex.: $conn->run("SELECT title FROM qutes WHERE id = ?", [43]);
+     *  
+     * @param  string     $sql
+     * @param  array|null $params
+     * 
+     * @return mixed. Depends on query type
+     */
+    public function run(string $sql, array $params = null) 
+    {
+        if($this->pdo instanceof \PDO)
+        {
+        
+            try {
+                
+                $stmt = $this->pdo->prepare($sql);
+                
+                $stmt->execute($params);
+                
+                $query = explode(" ", strtolower($sql));
+            
+                if ($query[0] === 'select' || $query[0] === 'show') 
+                {
+                    return $stmt->fetchAll();
+                } 
+                elseif ($query[0] === 'insert' || $query[0] === 'update' || $query[0] === 'delete') 
+                {
+                    return $stmt->rowCount();
+                } 
+                else {
+                    return NULL;
+                }
+            }
+            catch (\PDOException $e)
+            {
+                //Logger in dev or prod. mode
+                if($this->dev)
+                {
+                    print($e->getMessage());
+                }
+                else
+                {
+                    $f = fopen('db_logger.txt', "a", dirname(__FILE__));
+                    fwrite($f, $e->getMessage() . "\r\n");
+                    fclose($f);
+                }
+            }
+        }    
+    }
 }
 
